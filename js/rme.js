c963db9f04d22c75cbaadaeba1072b8d5feaf7f0
tomgdow <thomasgdowling@gmail.com>
2020-02-25 15:44:22 +0000

    add nine html_partials and openAll,closeAll buttons

--- js/rme.js
+++ js/rme.js
@@ -1,8 +1,18 @@
 (function () {
     'use strict';
     var pageCounter = 0;
-    const pageOrder = ["mime_rme.html", "rme_1_script_tag.html", "rme_2_noscript_tag.html",
-        "rme_3_link.html"];
+    // sectionForward();
+    const pageOrder = ["mime_rme.html", 
+                       "1_rme_script_tag.html",
+                       "2_rme_noscript_tag.html", 
+                       "3_rme_link.html",
+                       "4_meta_tag.html", 
+                       "5_doctype_and_html_tag.html",
+                       "6_head_tag.html",
+                       "7_body_tag.html",
+                       "8_closing_tag.html",
+                       "9_entities.html",
+                       "10_comments.html"];
     //Functions
     function insertContents(cb) {
         return function insert() {
@@ -14,6 +24,7 @@
             document.getElementsByClassName('syntax')[0].innerHTML = htmlDoc.body.firstElementChild.children[3].innerHTML;
             document.getElementsByClassName('examples')[0].innerHTML = htmlDoc.body.firstElementChild.children[4].innerHTML;
             document.getElementsByClassName('notes')[0].innerHTML = htmlDoc.body.firstElementChild.children[5].innerHTML;
+            document.getElementsByClassName('file_name')[0].innerHTML=pageOrder[pageCounter];
             cb();
             console.log(htmlDoc.body.firstElementChild.children[1].innerHTML);
         };
@@ -35,6 +46,7 @@
         if (pageCounter < pageOrder.length-1) { ++pageCounter; }
         makeRequest(`./html/${pageOrder[pageCounter]}`, insertContents(callback));
         // document.getElementsByClassName('counter')[0].innerHTML=pageCounter+1;
+        document.getElementsByClassName('file_name')[0].innerHTML=pageOrder[pageCounter];
 
     }
 
@@ -42,12 +54,31 @@
         if (pageCounter > 0) {
             --pageCounter;
             makeRequest(`./html/${pageOrder[pageCounter]}`, insertContents(callback));
+            document.getElementsByClassName('file_name')[0].innerHTML=pageOrder[pageCounter];
         }
     }
+
+    function openAll () {
+        let temp=document.getElementsByTagName('details');
+        for(let i=0; i< temp.length; i++) {
+            temp[i].open="open";
+        }
+
+    }
+    function closeAll () {
+        let temp=document.getElementsByTagName('details');
+        for(let i=0; i< temp.length; i++) {
+            temp[i].removeAttribute('open');
+        }
+
+    }
+    
 
     // Event Listeners
     document.getElementById('btn-forward').addEventListener('click', sectionForward);
     document.getElementById('btn-back').addEventListener('click', sectionBack);
+    document.getElementById('open_all').addEventListener('click', openAll);
+    document.getElementById('close_all').addEventListener('click', closeAll);
 
     // Event Listeners
     window.addEventListener('DOMContentLoaded', function () {
